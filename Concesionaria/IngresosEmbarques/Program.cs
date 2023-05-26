983b62c35e44cc36b7225134a41ddf40da065a34
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2023-05-26 17:44:19 -0300

    mejoras en la configuración y comentarios

--- Concesionaria/IngresosEmbarques/Program.cs
+++ Concesionaria/IngresosEmbarques/Program.cs
@@ -8,11 +8,23 @@ namespace IngresosEmbarques
 {
     class Program
     {
+        static int ImprimirConfiguración()
+        {
+            Console.Clear();
+            Console.WriteLine("\t\t Sistema de ingreso de embarque de motos a existencias.\n");
+            
+            Console.WriteLine("Solicitar año actual.");
+            int añoActual = Convert.ToInt32(Console.ReadLine());
+
+            return añoActual;
+        }
+
         static void ImprimirMenu()
         {
             Console.Clear();
-            Console.WriteLine("1-agregar embarque");
-            Console.WriteLine("2-Consultar estado concesionaria");
+            Console.WriteLine("\t\t Sistema de ingreso de embarque de motos a existencias.\n");
+            Console.WriteLine("1- Agregar embarque");
+            Console.WriteLine("2- Consultar estado concesionaria");
             Console.WriteLine("otro - salir");
         }
 
@@ -36,10 +48,8 @@ namespace IngresosEmbarques
             #endregion
 
             #region solicitar año actual
-            Console.WriteLine("Solicitar año actual.");
-            añoActual = Convert.ToInt32(Console.ReadLine());
+            añoActual = ImprimirConfiguración();
             #endregion
-
 
             #region iniciar var...
             cantEmbarque = 0;
@@ -102,7 +112,8 @@ namespace IngresosEmbarques
                             Console.WriteLine("Resumen del embarque");
 
                             #region informar el total del embarque
-                            Console.WriteLine("Monto total del embarque ingresado es: ${0:f2}", montoXEmbarque);
+                            Console.WriteLine("Monto total del embarque (número: {1}) ingresado es: ${0:f2}", 
+                                                                        montoXEmbarque, nroEmbarque);
                             #endregion
 
                             #region actualizar el monto X embarque
@@ -120,6 +131,7 @@ namespace IngresosEmbarques
                             Console.Clear();
 
                             // mostrar resultados finales de caja
+
                             #region informar el total de todos los embarques y cantidad de embarques
                             Console.WriteLine("Monto total de todos los embarques ingresados es: ${0:f2}", montoEmbarques);
                             Console.WriteLine("Cantidad de embarques: {0}", cantEmbarque);
@@ -132,7 +144,6 @@ namespace IngresosEmbarques
                 }
             
             } while (op>0);
-
         }
     }
 }
