7259ce2ece9a78839414608d06c16c3a443afbef
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2023-05-25 21:52:27 -0300

    agregando la clase concesionaria

---
+++ Concesionaria/IngresosEmbarques/Concesionaria.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IngresosEmbarques
+{
+    class Concesionaria
+    {
+        #region variables del embarques
+        int añoActual;
+
+        public double MontoEmbarques { get; set; }
+
+        #endregion
+
+        #region xEmbarque
+        public int nroEmbarque;
+        public double montoXEmbarque;
+        #endregion
+
+        public void Inicializar(int añoActual)
+        {
+            this.añoActual = añoActual;
+            cantEmbarque = 0;
+            MontoEmbarques = 0;
+        }
+
+        public void InicializarEmbarque(int nroEmbarque)
+        {
+            this.nroEmbarque = nroEmbarque;
+            montoXEmbarque = 0;
+        }
+
+        public double AgregarMoto(int modelo, double valorFabricacion)
+        {
+            double valorActual=0d;
+
+            #region calcular monto moto
+            valorActual = valorFabricacion * Math.Pow(0.9, añoActual - modelo);
+            #endregion
+
+            #region actualizar el monto X embarque
+            montoXEmbarque += valorActual;
+            #endregion
+
+            return valorActual;
+        }
+
+        public void FinalizarEmbarque()
+        {            
+            cantEmbarque++;
+
+            #region actualizar el monto X embarque
+            MontoEmbarques += montoXEmbarque;
+            #endregion            
+        }
+    }
+}
--- Concesionaria/IngresosEmbarques/Program.cs
+++ Concesionaria/IngresosEmbarques/Program.cs
@@ -18,32 +18,15 @@ namespace IngresosEmbarques
 
         static void Main(string[] args)
         {
-            #region variables del embarques
-            int añoActual=0;
-            double montoEmbarques;
-            int cantEmbarque;
-            #endregion
-
-            # region xEmbarque
-            int nroEmbarque;
-            double montoXEmbarque;
-            #endregion
-
-            #region xMoto
-            int modelo;
-            double valorFabricacion;
-            double valorActual;
-            #endregion
+            Concesionaria c = new Concesionaria();
 
             #region solicitar año actual
             Console.WriteLine("Solicitar año actual.");
-            añoActual = Convert.ToInt32(Console.ReadLine());
+            int añoActual = Convert.ToInt32(Console.ReadLine());
             #endregion
 
-
             #region iniciar var...
-            cantEmbarque = 0;
-            montoEmbarques = 0;
+            c.Inicializar(añoActual);
             #endregion
 
             int op;
@@ -60,54 +43,42 @@ namespace IngresosEmbarques
 
                             #region solicitar nro embarque
                             Console.WriteLine("Solicitar Número embarque (0 corta).");
-                            nroEmbarque = Convert.ToInt32(Console.ReadLine());
+                            int nroEmbarque = Convert.ToInt32(Console.ReadLine());
+                            c.InicializarEmbarque(nroEmbarque);
                             #endregion
 
                             // proceso ingresar el embarque de motos
                             #region ingresar un embarque
 
                             #region solicitar modelo
-                            Console.WriteLine("Solicitar Modelo moto (0 corta)");
-                            modelo = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Solicitar Modelo moto");
+                            int modelo = Convert.ToInt32(Console.ReadLine());
                             #endregion
-
-                            cantEmbarque++;
-
-                            #region iniciar variables
-                            montoXEmbarque = 0;
-                            #endregion
-
+                                                        
                             while (modelo > 0) //xmoto
                             {
                                 #region solicitar valor de fabricacion
                                 Console.WriteLine("Solicitar valor fabriación $.");
-                                valorFabricacion = Convert.ToInt32(Console.ReadLine());
+                                double valor = Convert.ToInt32(Console.ReadLine());
                                 #endregion
 
-                                #region calcular monto moto
-                                valorActual = valorFabricacion * Math.Pow(0.9, añoActual - modelo);
-                                #endregion
-
-                                #region actualizar el monto X embarque
-                                montoXEmbarque += valorActual;
-                                #endregion
-
+                                c.AgregarMoto(modelo, valor);
+                                
                                 #region solicitar modelo
                                 Console.WriteLine("Solicitar Modelo moto (0 corta)");
                                 modelo = Convert.ToInt32(Console.ReadLine());
                                 #endregion
                             }
 
+                            c.FinalizarEmbarque();
+
                             Console.Clear();
                             Console.WriteLine("Resumen del embarque");
 
                             #region informar el total del embarque
-                            Console.WriteLine("Monto total del embarque ingresado es: ${0:f2}", montoXEmbarque);
+                            Console.WriteLine("Monto total del embarque ingresado es: ${0:f2}", c.montoXEmbarque);
                             #endregion
-
-                            #region actualizar el monto X embarque
-                            montoEmbarques += montoXEmbarque;
-                            #endregion
+                                                        
                             #endregion
                             // informar los resultados
 
@@ -121,8 +92,8 @@ namespace IngresosEmbarques
 
                             // mostrar resultados finales de caja
                             #region informar el total de todos los embarques y cantidad de embarques
-                            Console.WriteLine("Monto total de todos los embarques ingresados es: ${0:f2}", montoEmbarques);
-                            Console.WriteLine("Cantidad de embarques: {0}", cantEmbarque);
+                            Console.WriteLine("Monto total de todos los embarques ingresados es: ${0:f2}", c.MontoEmbarques);
+                            Console.WriteLine("Cantidad de embarques: {0}", c.cantEmbarque);
                             #endregion
 
                             Console.WriteLine("Presione una tecla para volver al menú");
